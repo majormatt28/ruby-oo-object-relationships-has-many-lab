afaf53eed6c3f59541669bfdee8179531f2e4ce7
Matthew <elijah2895@hotmail.com>
2021-02-27 20:25:20 -0500

    commit tree 24174b1626f3c263b41cba26ae1311c013dc34dd

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,34 @@
 class Author
+    attr_accessor :name
+    @@post_count = 0
+
+    def initialize(name)
+        @name = name
+        @posts = []
+        
+    end
+
+    def posts
+        Post.all.select do |post|
+            post.author == self
+        end
+    end
+
+    def add_post(post)
+        self.posts << post
+        post.author = self
+
+    end
+
+    def add_post_by_title(post_title)
+        post = Post.new(post_title)
+        self.posts << post
+        post.author = self
+    end
+
+    def self.post_count
+        @@post_count += 3
+    end
+
 
 end
